8e87c04040961aef2d4fb3ac9545440db132e024
arvianOnzy <arvianilmi55@gmail.com>
2022-07-27 14:36:39 +0700

    db nyar

--- dms_ims.sql
+++ dms_ims.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2022 at 08:00 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.15
+-- Generation Time: Jul 27, 2022 at 09:35 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_peminjaman`
+-- Table structure for table `dokumen_master`
 --
 
-CREATE TABLE `data_peminjaman` (
-  `id_peminjaman` bigint(20) NOT NULL,
-  `id_pengembalian` bigint(20) DEFAULT NULL,
-  `tgl_peminjaman` timestamp(6) NULL DEFAULT NULL,
-  `tgl_pengembalian` timestamp(6) NULL DEFAULT NULL,
-  `batas_peminjaman` timestamp(6) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `dokumen_master` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_dok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_dok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenisdok_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ruangan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rak` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kardus` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `dokumen_master`
+--
+
+INSERT INTO `dokumen_master` (`id`, `nama_dok`, `no_dok`, `jenisdok_id`, `ruangan`, `rak`, `kardus`, `gambar`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(1, 'plagiat', '1', '1', 'it', '1', 'arsip', 'img-dokumen/VFJTBLjwmVKCasv8OyqnM6yJOUTLtlbsZWzCgcvF.jpg', NULL, '2022-07-27 00:28:12', '2022-07-27 00:28:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -42,121 +71,405 @@ CREATE TABLE `data_peminjaman` (
 --
 
 CREATE TABLE `folder` (
-  `folder_id` int(255) NOT NULL,
-  `tree_id` int(255) DEFAULT NULL,
-  `nama_folder` varchar(1000) DEFAULT NULL,
-  `unit_id` int(255) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT NULL,
-  `updated_at` timestamp(6) NULL DEFAULT NULL,
-  `deleted_` timestamp(6) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jenis_doc`
---
-
-CREATE TABLE `jenis_doc` (
-  `id` int(100) NOT NULL,
-  `jenis` int(11) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT NULL,
-  `updated_at` timestamp(6) NULL DEFAULT NULL,
-  `deleted_at` timestamp(6) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lokasi_doc`
---
-
-CREATE TABLE `lokasi_doc` (
-  `loc_id` int(100) NOT NULL,
-  `lokasi` int(11) DEFAULT NULL,
-  `created_at` timestamp(6) NULL DEFAULT NULL,
-  `updated_at` timestamp(6) NULL DEFAULT NULL,
-  `deleted_at` timestamp(6) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tahapan`
---
-
-CREATE TABLE `tahapan` (
-  `tahapan_id` int(100) NOT NULL,
-  `nama_tahapan` varchar(1000) DEFAULT NULL,
-  `unit` varchar(1000) DEFAULT NULL,
-  `urutan` varchar(100) DEFAULT NULL,
-  `pangajuan_id` int(100) DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tree_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `folder` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_dokumen`
+--
+
+CREATE TABLE `jenis_dokumen` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `jenis` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dokumen_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `jenis_dokumen`
+--
+
+INSERT INTO `jenis_dokumen` (`id`, `jenis`, `dokumen_id`, `created_at`, `updated_at`) VALUES
+(1, 's', NULL, '2022-07-27 00:27:49', '2022-07-27 00:27:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `ruangan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rak` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kardus` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasis`
+--
+
+CREATE TABLE `lokasis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_07_05_064532_jenis_dokumen', 1),
+(6, '2022_07_06_084649_dokumen_master', 1),
+(7, '2022_07_08_003332_lokasi', 1),
+(8, '2022_07_08_003713_create_lokasis_table', 1),
+(9, '2022_07_13_015056_permintaan', 1),
+(10, '2022_07_20_015637_role', 1),
+(11, '2022_07_26_015114_tahapan_dtl', 1),
+(12, '2022_07_26_015440_tahapan_hdr', 1),
+(13, '2022_07_26_015856_folder', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permintaan`
+--
+
+CREATE TABLE `permintaan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_dok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_dok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenisdok_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `permintaan`
+--
+
+INSERT INTO `permintaan` (`id`, `nama_dok`, `no_dok`, `jenisdok_id`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(1, 'plagiat', '1', '1', NULL, '2022-07-27 00:28:34', '2022-07-27 00:28:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `nama`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(1, 'admin', NULL, NULL, NULL),
+(2, 'member', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tahapan_dtl`
+--
+
+CREATE TABLE `tahapan_dtl` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tahapan_hdr`
+--
+
+CREATE TABLE `tahapan_hdr` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `master_tahapan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nip` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT 1,
+  `darkMode` int(11) NOT NULL DEFAULT 0,
+  `openSideBar` int(11) NOT NULL DEFAULT 1,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '$2y$10$JUUKFmyB9pWCIAcTjC12QOT5XdV7t8peTA3.zbNvaUUj.pwGwBVq.',
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nip`, `name`, `email`, `role_id`, `darkMode`, `openSideBar`, `password`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'admin', 'admin@ims.icon', 1, 0, 1, '$2y$10$ic3kASGQ9ZWUYMDH7BO4LuPbYjJICGmyY2HEDr3bsB3z/CwH1fty.', NULL, NULL, '2022-07-27 00:07:30', '2022-07-27 00:07:30');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `data_peminjaman`
+-- Indexes for table `dokumen_master`
 --
-ALTER TABLE `data_peminjaman`
-  ADD PRIMARY KEY (`id_peminjaman`);
+ALTER TABLE `dokumen_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
 -- Indexes for table `folder`
 --
 ALTER TABLE `folder`
-  ADD PRIMARY KEY (`folder_id`);
-
---
--- Indexes for table `jenis_doc`
---
-ALTER TABLE `jenis_doc`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lokasi_doc`
+-- Indexes for table `jenis_dokumen`
 --
-ALTER TABLE `lokasi_doc`
-  ADD PRIMARY KEY (`loc_id`);
+ALTER TABLE `jenis_dokumen`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tahapan`
+-- Indexes for table `lokasi`
 --
-ALTER TABLE `tahapan`
-  ADD PRIMARY KEY (`tahapan_id`);
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lokasis`
+--
+ALTER TABLE `lokasis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `permintaan`
+--
+ALTER TABLE `permintaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tahapan_dtl`
+--
+ALTER TABLE `tahapan_dtl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tahapan_hdr`
+--
+ALTER TABLE `tahapan_hdr`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_nip_unique` (`nip`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `dokumen_master`
+--
+ALTER TABLE `dokumen_master`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `folder`
 --
 ALTER TABLE `folder`
-  MODIFY `folder_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `jenis_doc`
+-- AUTO_INCREMENT for table `jenis_dokumen`
 --
-ALTER TABLE `jenis_doc`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `jenis_dokumen`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `lokasi_doc`
+-- AUTO_INCREMENT for table `lokasi`
 --
-ALTER TABLE `lokasi_doc`
-  MODIFY `loc_id` int(100) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `lokasi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tahapan`
+-- AUTO_INCREMENT for table `lokasis`
 --
-ALTER TABLE `tahapan`
-  MODIFY `tahapan_id` int(100) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `lokasis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `permintaan`
+--
+ALTER TABLE `permintaan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tahapan_dtl`
+--
+ALTER TABLE `tahapan_dtl`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tahapan_hdr`
+--
+ALTER TABLE `tahapan_hdr`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
